7cc41af774ed4514c0d929192d1b8ed220212720
Paras <dudharejiyaparas@gmail.com>
2023-03-29 03:39:10 +0530

    Initial commit

---
+++ app/src/main/java/com/paras/demo_kotlin_appication/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.paras.demo_kotlin_appication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.FragmentContainer
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var  navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNavigationView  = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        setupWithNavController(bottomNavigationView , navController)
+
+
+
+    }
+
+    private fun setupWithNavController() {
+        TODO("Not yet implemented")
+    }
+}
+
+
